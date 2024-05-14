142caacc5b85f3cf593f1b60ae647c0cee0e9e1d
Ibrahim-Abdikadir <ibrasayd48@gmail.com>
2024-05-14 16:26:32 +0300

    Properties

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import properties from '@/properties.json'
 
 const PropertiesPage = () => {
   return (
-    <div>PropertiesPage</div>
+    <div>
+      <section className='px-4 py-6'>
+        <div className='container bg-red-400 px-4 py-6'>
+          <div>
+
+          </div>
+        </div>
+      </section>
+    </div>
   )
 }
 
